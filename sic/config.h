e317bed710a06957e8a0d2c71c96ce445ba15d37
Koragg <31944041+KoraggKnightWolf@users.noreply.github.com>
2023-09-28 17:50:54 +0200

    Add files via upload

---
+++ sic/config.h
@@ -0,0 +1,17 @@
+/* Host used when "-h" is not given */
+#define DEFAULT_HOST "localhost"
+
+/* Port used when "-p" is not given */
+#define DEFAULT_PORT "6667"
+
+/* Timestamp format; see strftime(3). */
+#define TIMESTAMP_FORMAT "%Y-%m-%d %R"
+
+/* Command prefix character. In most IRC clients this is '/'. */
+#define COMMAND_PREFIX_CHARACTER '/'
+
+/* Parting message used when none is specified with ":l ..." command. */
+#define DEFAULT_PARTING_MESSAGE "Leaving"
+
+/* Suffix character used to print the current inserting command */
+#define LINEPRINT_SUFFIX_CHAR '\\'
